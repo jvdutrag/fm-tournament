e62952adef6d4f4a2cd886eb327baa87da5dd40e
João Dutra <joaovitordutrag@gmail.com>
2024-04-20 20:18:39 -0300

    add winning

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { Bracket } from "react-brackets";
+import { Bracket, Seed, SeedItem, SeedTeam } from "react-brackets";
 
 import upper from "./status/upper.json";
 import lower from "./status/lower.json";
 
-import config from "./config.json";
-
 import "./App.css";
+
+function CustomSeed({ seed, breakpoint }) {
+  const hasWon = (teamName) => teamName === seed.winner_name;
+
+  return (
+    <Seed mobileBreakpoint={breakpoint}>
+      <SeedItem>
+        {seed.teams.map((team, index) => (
+          <SeedTeam
+            key={index}
+            style={{
+              color: hasWon(team.name) ? "green" : undefined
+            }}
+          >
+            {team.name}
+          </SeedTeam>
+        ))}
+      </SeedItem>
+    </Seed>
+  );
+}
 
 export default function App() {
   return (
@@ -13,17 +32,14 @@ export default function App() {
       <h1 style={{ textAlign: "center" }}>
         CELLBIT'S FIRST FRUIT MOUNTAIN TOURNAMENT
       </h1>
-      <h2 style={{ textAlign: "center" }}>
-        Current Round: {config.currentRound}
-      </h2>
       <div>
         <h1>Upper Bracket</h1>
-        <Bracket rounds={upper} />
+        <Bracket rounds={upper} renderSeedComponent={CustomSeed} />
       </div>
       <div>
         <h1>Lower Bracket</h1>
         <Bracket rounds={lower} />
       </div>
     </div>
-  )
+  );
 }
